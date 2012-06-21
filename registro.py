4b78afb51b196ce59464b387e541cfecae8855dd
Sergio Oliveira <sergio@tracy.com.br>
2012-06-21 09:48:15 -0300

    Movendo specs dos registros para o modulo registro

--- registro.py
+++ registro.py
@@ -1,8 +1,13 @@
 
-import cnab240
-
-from decimal import Decimal
+import os                                                                        
+import json                                                                      
+import errors
+                                                                                 
+from decimal import Decimal, InvalidOperation
 from collections import OrderedDict
+
+REGISTRO_SPECS = {}
+
 
 class Campo(object):
     def __init__(self, spec):
@@ -13,15 +18,14 @@ class Campo(object):
         self.formato = spec.get('formato', 'alfa')
         self.decimais = spec.get('decimais', 0)
         self.default = spec.get('default', None)
-            
+        
         if not self.default:
-            if self.formato == 'num':
-                if self.decimais:
-                    self.default = Decimal('0' * self.digitos) 
-                else:
-                    self.default = 0 
-            else:
+            if self.formato == 'alfa':
                 self.default = u' ' * self.digitos
+            elif self.decimais:
+                self.default = Decimal('0' * self.digitos) 
+            else:
+                self.default = 0 
 
         self._valor = None
     
@@ -31,22 +35,30 @@ class Campo(object):
 
     @valor.setter
     def valor(self, valor):
-        if self.formato == 'num':
-            if self.decimais:
-                if isinstance(valor, Decimal):
-                    if valor.as_tuple().exponent == self.digitos * -1:
-                        self._valor = valor
-                    else:
-                        raise Exception # TODO
-                else:
-                    raise Exception # TODO 
-            else:
-                if isinstance(valor, int):
-                    self._valor = valor
-                else:
-                    raise Exception #TODO
+        if self.formato == 'alfa':
+            if not isinstance(valor, unicode):
+                raise errors.TipoError(self, valor)
+            if len(valor) > self.digitos:
+                raise errors.NumDigitosExcedidoError(self, valor)
+
+        elif self.decimais:
+            if not isinstance(valor, Decimal):
+                raise errors.TipoError(self, valor)
+            
+            num_decimais = valor.as_tuple().exponent * -1
+            if num_decimais != self.decimais:
+                raise errors.NumDecimaisError(self, valor)
+            
+            if len(str(valor).replace('.', '')) > self.digitos:
+                raise errors.NumDigitosExcedidoError(self, valor)
+
         else:
-            self._valor = valor
+            if not isinstance(valor, (int, long)):
+                raise errors.TipoError(self, valor)
+            if len(str(valor)) > self.digitos:
+                raise errors.NumDigitosExcedidoError(self, valor)
+        
+        self._valor = valor
 
     def __unicode__(self):
         if self._valor:
@@ -72,17 +84,11 @@ class Campo(object):
         return self.valor
 
 
-class ErrorAoLerCampo(Exception):
-    #def __unicode__(self):
-    #    TODO 
-    pass
-
-
 class Registro(object):
 
     def __new__(cls, nome):
         
-        spec = cnab240.registro_specs.get(nome)
+        spec = REGISTRO_SPECS.get(nome, {})
         campos = OrderedDict()
         attrs = {'_campos': campos}
 
@@ -98,30 +104,38 @@ class Registro(object):
 
 class RegistroBase(object):
 
-    def carrega(self, registro_str):
-        """
-        Exemplo de registro_str: 
-00100000         212345678901234abcdefghijklmnopqrst028916000000014262X7TRACY TECNOLOGIA LTDA ME      BANCO DO BRASIL                         02012071322000012345608512345                                                                     
-"""       
+    def carregar(self, registro_str):
  
         for campo in self._campos.values():
             
             valor = registro_str[campo.inicio:campo.fim].strip()
             if campo.formato == 'num' and campo.decimais:
+                dec = valor[:campo.decimais] + '.' + valor[campo.decimais:]
                 try:
-                    dec = valor[:campo.decimais] + '.' + valor[campo.decimais:]
                     campo.valor = Decimal(dec)
                 except InvalidOperation:
-                    raise ErrorAoLerCampo('Convertendo para decimal', 
-                                          valor, campo_spec)
+                    raise # raise custom? 
 
             elif campo.formato == 'num':
                 try:
                     campo.valor = int(valor)
                 except ValueError:
-                    raise ErrorAoLerCampo('Convertendo para inteiro',
-                                          valor, campo_spec)
+                    raise # raise custom? 
+            else:
+                campo.valor = valor
 
     def escreve(self):
         return ''.join([unicode(campo) for campo in self._campos.values()])  
+
+
+cwd_path = os.path.abspath(os.path.dirname(__file__))                            
+registros_dirpath = os.path.join(cwd_path, 'registros')                          
+for registro_path in os.listdir(registros_dirpath):                              
+    registro_file = open(os.path.join(registros_dirpath, registro_path))         
+    # TODO: Validar spec: nome (deve ser unico para cada registro), 
+    #   posicao_inicio, posicao_fim, formato (alpha), decimais (0), 
+    #   default (zeros se numerico ou brancos se alfa)                           
+    spec = json.load(registro_file)                                              
+    REGISTRO_SPECS.update({os.path.splitext(registro_path)[0]: spec})            
+    registro_file.close()      
 
