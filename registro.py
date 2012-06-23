7f82dd820505d3ca2a5582584cba6eb1fbd36dfb
Sergio Oliveira <sergio@tracy.com.br>
2012-06-23 17:47:26 -0300

    Carregando arquivos de specs usando versoes

--- registro.py
+++ registro.py
@@ -1,8 +1,9 @@
 
-import os                                                                        
-import json                                                                      
+import os
+import json
 import errors
-                                                                                 
+
+from glob import glob
 from decimal import Decimal, InvalidOperation
 from collections import OrderedDict
 from campo import Campo
@@ -14,7 +15,7 @@ REGISTRO_SPECS = {}
 class Registro(object):
 
     def __new__(cls, nome):
-        
+
         spec = REGISTRO_SPECS.get(nome, {})
         campos = OrderedDict()
         attrs = {'_campos': campos}
@@ -26,15 +27,15 @@ class Registro(object):
             attrs.update({campo.nome: campo})
 
         new_cls = type('Registro', (RegistroBase, ), attrs)
-        return new_cls() 
+        return new_cls()
 
 
 class RegistroBase(object):
 
     def carregar(self, registro_str):
- 
+
         for campo in self._campos.values():
-            
+
             valor = registro_str[campo.inicio:campo.fim].strip()
             if campo.formato == 'num' and campo.decimais:
                 exponente = campo.decimais * -1
@@ -42,7 +43,7 @@ class RegistroBase(object):
                 try:
                     campo.valor = Decimal(dec)
                 except InvalidOperation:
-                    raise # raise custom? 
+                    raise # raise custom?
 
             elif campo.formato == 'num':
                 try:
@@ -53,17 +54,24 @@ class RegistroBase(object):
                 campo.valor = valor
 
     def escreve(self):
-        return ''.join([unicode(campo) for campo in self._campos.values()])  
+        return ''.join([unicode(campo) for campo in self._campos.values()])
 
 
-cwd_path = os.path.abspath(os.path.dirname(__file__))                            
-registros_dirpath = os.path.join(cwd_path, 'registros')                          
-for registro_path in os.listdir(registros_dirpath):                              
-    registro_file = open(os.path.join(registros_dirpath, registro_path))         
-    # TODO: Validar spec: nome (deve ser unico para cada registro), 
-    #   posicao_inicio, posicao_fim, formato (alpha), decimais (0), 
-    #   default (zeros se numerico ou brancos se alfa)                           
-    spec = json.load(registro_file)                                              
-    REGISTRO_SPECS.update({os.path.splitext(registro_path)[0]: spec})            
-    registro_file.close()      
+def load_register_specs():
+    # TODO: Validar spec: nome (deve ser unico para cada registro),
+    #   posicao_inicio, posicao_fim, formato (alpha), decimais (0),
+    #   default (zeros se numerico ou brancos se alfa)
+    cwd_path = os.path.abspath(os.path.dirname(__file__))
+    specs_dirpath = os.path.join(cwd_path, 'registros')
+    for version in os.listdir(specs_dirpath):
+        specs = {}
+        REGISTRO_SPECS.update({version: specs})
+        registro_filepath_list = glob(os.path.join(specs_dirpath,
+                                                    version, '*.json'))
+        for registro_filepath in registro_filepath_list:
+            registro_file = open(registro_filepath)
+            spec = json.load(registro_file)
+            specs.update({spec.get('nome'): spec})
+            registro_file.close()
 
+load_register_specs()
