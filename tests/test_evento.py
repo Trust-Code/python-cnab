b8d90a6a2a59ded3ea427e39a048eb9f26036aae
Sergio Oliveira <sergio@tracy.com.br>
2012-06-26 08:38:15 -0300

    Removendo testes para evento e lote

--- tests/test_evento.py
+++ tests/test_evento.py
@@ -1,25 +1,5 @@
 
 import unittest
 
-from cnab240.evento import Evento
-from cnab240.registro import Registro
-from tests.data import REGISTRO_T, EventoImplementado
-
-
 class TestEvento(unittest.TestCase):
-    
-    def test_not_implemented(self):
-        with self.assertRaises(NotImplementedError):
-            evento = Evento(versao='085')
-
-    def test_segmento_invalido(self):
-        evento = EventoImplementado(versao='085') 
-        with self.assertRaises(TypeError):
-            evento.adicionar_segmento('evento')
-
-    def test_segmento_valido(self):
-        evento = EventoImplementado(versao='085') 
-        registro = Registro('segmento_t', '085')
-        registro.carregar(REGISTRO_T) 
-        evento.adicionar_segmento(registro)
-        self.assertEqual(evento._segmentos['T'], registro)
+   pass 
--- tests/test_lote.py
+++ tests/test_lote.py
@@ -1,34 +1,5 @@
 
 import unittest
 
-from cnab240 import errors
-from cnab240.lote import Lote
-from cnab240.registro import Registro 
-from tests.data import REGISTRO_T, EventoImplementado, LoteImplementado
-
 class TestLote(unittest.TestCase):
-
-    def test_lote_invalido(self):
-        with self.assertRaises(NotImplementedError):
-            lote = Lote(versao='085')
-
-    def test_adicionar_evento_invalido(self):
-        lote = LoteImplementado(versao='085')
-        with self.assertRaises(TypeError):
-            lote.adicionar_evento('evento')
-
-    def test_adicionar_evento(self):
-        registro = Registro('segmento_t', '085')
-        registro.carregar(REGISTRO_T)
-        evento = EventoImplementado(versao='085')
-        evento.adicionar_segmento(registro) 
-        lote = LoteImplementado(versao='085')
-        lote.adicionar_evento(evento)
-        
-        self.assertEqual(lote._eventos[0], evento)
-        
-        evento2 = EventoImplementado(versao='085')
-        self.assertNotEqual(lote._eventos[0], evento2)
-    
-if __name__ == '__main__':
-    unittest.main()
+    pass
