59296d5141dca56b4aea0ed866c0227c2bea95e8
Sergio Oliveira <sergio@tracy.com.br>
2012-07-02 12:48:54 -0300

    Removendo testes desatualizados

--- tests/test_arquivo.py
+++ tests/test_arquivo.py
@@ -2,22 +2,12 @@
 import unittest
 
 from cnab240 import errors
-from cnab240.bancos.bb.tipos import Arquivo
 
 
 class TestCnab240(unittest.TestCase):
 
     def setUp(self):
-        self.arquivo_bb = Arquivo()
-        
-    def test_nenhum_lote(self):
-        with self.assertRaises(errors.ArquivoVazioError):
-            unicode(self.arquivo_bb)
-
-    def test_erro_de_lote(self):
-        with self.assertRaises(TypeError):
-            self.arquivo_bb.adicionar_lote('Lote')
-
+        pass
 
 if __name__ == '__main__':
     unittest.main()
