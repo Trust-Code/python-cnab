5a98f8c013213b66b4afc8caaa4eb3655c3c7384
Sergio Oliveira <sergio@tracy.com.br>
2012-07-03 17:10:56 -0300

    Novas funcionalidades para os tipos e testes para lote

--- cnab240/tipos.py
+++ cnab240/tipos.py
@@ -4,118 +4,13 @@ from datetime import datetime
 from cnab240 import errors
 
 
-class ArquivoBase(object):
-
-    banco = None
-
-    def __init__(self, **kwargs):
-        """Arquivo Cnab240. 
-      
-        :param empresa_inscricao_tipo: Tipo de Inscrição da Empresa
-        :type empresa_inscricao_tipo: int
-
-        :param empresa_inscricao_numero: Número de Inscrição da Empresa
-        :type empresa_inscricao_numero: int
-
-        :param empresa_convenio: Código do Convênio no Banco
-        :type :empresa_convenio: str
-
-        :param empresa_conta_agencia_codigo: Agência Mantenedora da Conta
-        :type empresa_conta_agencia_codigo: int
-
-        :param empresa_conta_agencia_dv: Dígito Verificador da Agência
-        :type empresa_conta_agencia_dv: str 
-
-        :param empresa_conta_numero: Número da Conta Corrente
-        :type empresa_conta_numero: int
-
-        :param empresa_conta_dv: Dígito Verificador da Conta
-        :type empresa_conta_dv: str 
-
-        :param empresa_agencia_conta_dv: Dígito Verificador da Ag/Conta
-        :type empresa_agencia_conta_dv: str
-
-        :param empresa_nome: Nome da Empresa
-        :type empresa_nome: str
-
-        :param nome_do_banco: Nome do Banco
-        :type nome_do_banco: str
-
-        :param arquivo_codigo: Código Remessa / Retorno
-        :type arquivo_codigo: int
-
-        :param arquivo_data_de_geracao: Data de Geração do Arquivo
-        :type arquivo_data_de_geracao: int
-
-        :param arquivo_hora_de_geracao: Hora de Geração do Arquivo
-        :type arquivo_hora_de_geracao: int
-
-        :param arquivo_sequencia: Número Seqüencial do Arquivo
-        :type arquivo_sequencia: int
-
-        :param arquivo_layout: No da Versão do Layout do Arquivo
-        :type arquivo_layout: int
-
-        :param arquivo_densidade: Densidade de Gravação do Arquivo
-        :type arquivo_densidade: int
-
-        :param reservado_banco: Para Uso Reservado do Banco
-        :type reservado_banco: str
-
-        :param reservado_empresa: Para Uso Reservado da Empresa
-        :type reservado_empresa: str
-
-        """
-
-        self.header = self.banco.registros.HeaderArquivo()
-        self.header.fromdict(kwargs)
-        
-        self.trailer = self.banco.registros.TrailerArquivo()
-        self.trailer.fromdict(kwargs)        
-        
-        self._lotes = []
-
-    @property
-    def lotes(self):
-        return lotes
-
-    def __unicode__(self):
-        if not self._lotes:
-            raise errors.ArquivoVazioError()
-        
-        result = []
-        result.append(unicode(self.header))
-        result.extend(unicode(lote) for lote in self._lotes)
-        result.append(unicode(self.trailer))
-        return u'\n'.join(result)
-        
-    def adicionar_lote(self, lote):
-        if not isinstance(lote, 'Lote'):
-            raise TypeError('Objeto deve ser instancia de "Lote"')
-        self._lotes.append(lote)
-
-        # Incrementar numero de lotes no trailer do arquivo
-        self.trailer.totais_quantidade_lotes += 1
-
-        # Incrementar numero de registros no trailer do arquivo
-        for evento in lote.eventos:
-            self.trailer.totais_quantidade_registros += evento.segmentos.count()
-
-    def escrever(self, file_):
-        file_.write(unicode(self.header))
-        file_.write('\n')
-        for lote in self._lotes:
-            file_.write(unicode(lote))
-            file_.write('\n')
-        file_.write(unicode(self.trailer))
-        file_.write('\n')
-
-
 class EventoBase(object):
+
     def __init__(self, banco):
         self._segmentos = []
         self.banco = banco 
-       
+        self._codigo_lote = None      
+ 
     @property
     def segmentos(self):    
         return self._segmentos
@@ -129,33 +24,108 @@ class EventoBase(object):
     def __unicode__(self):
         return u'\n'.join(unicode(seg) for seg in self._segmentos)
 
+    def __len__(self):
+        return len(self._segmentos)
+
+    @property
+    def codigo_lote(self):
+        return self._codigo_lote
+
+    @codigo_lote.setter
+    def codigo_lote(self, valor):
+        self._codigo_lote = valor
+        for segmento in self._segmentos:
+            segmento.controle_lote = valor  
 
 class LoteBase(object):
 
-    eventos_validos = None
- 
-    def __init__(self, banco):
-        if self.eventos_validos is None:
-            raise NotImplementedError
-        
-        self.banco = banco
-    
-        self.header = self.banco.HeaderLote()
-        self.trailer = self.banco.TrailerLote()
+    def __init__(self, banco, header, trailer):
+        self.banco = banco 
+        self.header = header
+        self.trailer = trailer
+        self._codigo = None
+        self.trailer.quantidade_registros = 2
         self._eventos = []
-    
+
+    @property
+    def codigo(self):
+        return self._codigo   
+
+    @codigo.setter
+    def codigo(self, valor):
+        self._codigo = valor
+        self.header.controle_lote = valor
+        self.trailer.controle_lote = valor
+
+        for evento in self._eventos:
+            evento.codigo_lote = valor
+
+    @property
+    def eventos(self):
+        return self._eventos   
+ 
     def adicionar_evento(self, evento):
-        if any(isinstance(evento, cls) for cls in self.eventos_validos):
-            self._eventos.append(evento)
-        else:
+        if not isinstance(evento, EventoBase):
             raise TypeError
+        
+        self._eventos.append(evento)
+        self.trailer.quantidade_registros += len(evento)
 
     def __unicode__(self):
         if not self._eventos:
-            errors.NenhumEventoError()
+            raise errors.NenhumEventoError()
     
         result = [] 
         result.append(unicode(self.header))
         result.extend(unicode(evento) for evento in self._eventos)
         result.append(unicode(self.trailer))
         return '\n'.join(result)
+
+    def __len__(self):
+        return self.trailer.quantidade_registros
+
+
+class Arquivo(object):
+
+    def __init__(self, banco, **kwargs):
+        """Arquivo Cnab240.""" 
+
+        self._lotes = []
+        self.banco = banco
+            
+        self.header = self.banco.registros.HeaderArquivo(**kwargs) 
+        self.trailer = self.banco.registros.TrailerArquivo(**kwargs)
+        self.trailer.totais_quantidade_lotes = 0        
+        self.trailer.totais_quantidade_registros = 2
+        
+    @property
+    def lotes(self):
+        return lotes
+        
+    def adicionar_lote(self, lote):
+        if not isinstance(lote, LoteBase):
+            raise TypeError('Objeto deve ser instancia de "LoteBase"')
+
+        self._lotes.append(lote)
+        
+        lote.codigo = len(self._lotes)
+
+        # Incrementar numero de lotes no trailer do arquivo
+        self.trailer.totais_quantidade_lotes += 1
+
+        # Incrementar numero de registros no trailer do arquivo
+        self.trailer.totais_quantidade_registros += len(lote)
+
+    def escrever(self, file_):
+        file_.write(unicode(self.encode('ascii')))
+
+    def __unicode__(self):
+        if not self._lotes:
+            raise errors.ArquivoVazioError()
+        
+        result = []
+        result.append(unicode(self.header))
+        result.extend(unicode(lote) for lote in self._lotes)
+        result.append(unicode(self.trailer))
+        return u'\n'.join(result)
+
--- tests/test_lote.py
+++ tests/test_lote.py
@@ -1,5 +1,44 @@
 
 import unittest
 
+from cnab240 import errors
+from cnab240.bancos import itau
+from cnab240.lotes.cobranca import LoteCobranca 
+from cnab240.eventos.cobranca import EventoInclusao
+
+
 class TestLote(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.lote_itau = LoteCobranca(itau)
+        self.inclusao = EventoInclusao(itau) 
+
+    def test_init(self):
+        self.assertEqual(self.lote_itau.eventos, []) 
+        self.assertEqual(self.lote_itau.trailer.quantidade_registros, 2) 
+   
+    def test_adicionar_evento(self):
+        with self.assertRaises(TypeError):
+            self.lote_itau.adicionar_evento(None)
+
+        self.lote_itau.adicionar_evento(self.inclusao)
+        self.assertEqual(self.lote_itau.trailer.quantidade_registros, 4)
+        
+        self.lote_itau.adicionar_evento(self.inclusao)
+        self.assertEqual(self.lote_itau.trailer.quantidade_registros, 6)
+    
+    def test_unicode(self):
+        with self.assertRaises(errors.NenhumEventoError):
+            unicode(self.lote_itau)
+    
+    def test_definir_codigo(self):
+        self.lote_itau.adicionar_evento(self.inclusao)
+        self.lote_itau.codigo = 129 
+
+        self.assertEqual(self.lote_itau.header.controle_lote, 129)
+        self.assertEqual(self.lote_itau.trailer.controle_lote, 129)
+        for evento in self.lote_itau.eventos:
+            for seg in evento.segmentos:
+                self.assertEqual(seg.controle_lote, 129)
+    
+    
