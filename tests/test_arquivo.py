5082d4fb08f026f5f0892cd579bb99deb06878a2
Davi Campos <arlocm@gmail.com>
2012-07-10 13:42:30 -0300

    Corrigindo e finalizando alguns testes

--- tests/test_arquivo.py
+++ tests/test_arquivo.py
@@ -4,21 +4,22 @@ import unittest
 from cnab240 import errors
 from cnab240.bancos import itau
 from cnab240.tipos import Arquivo
-from tests.data import get_itau_data_from_file
+from tests.data import get_itau_data_from_dict, get_itau_file_remessa
 
 
 class TestCnab240(unittest.TestCase):
+    
+    def __init__(self, *args, **kwargs):
+        super(TestCnab240, self).__init__(*args, **kwargs)
+        self.maxDiff = None 
 
     def setUp(self):
-        self.itau_data = get_itau_data_from_file()
-        arquivo_args = self.itau_data['header_arquivo'].todict().items()
-        arquivo_args += self.itau_data['header_arquivo'].todict().items()
-        self.arquivo = Arquivo(itau, **dict(arquivo_args))
+        self.itau_data = get_itau_data_from_dict()
+        self.arquivo = Arquivo(itau, **self.itau_data['arquivo'])
 
     def test_unicode(self):
-        args_evento1 = self.itau_data['seg_p1'].todict().items()
-        args_evento1 += self.itau_data['seg_q1'].todict().items()
-        self.arquivo.incluir_cobranca(**dict(args_evento1))
+        self.arquivo.incluir_cobranca(**self.itau_data['cobranca'])
+        self.assertEqual(unicode(self.arquivo), get_itau_file_remessa())
 
     def test_empty_data(self):
         arquivo = Arquivo(itau)
