e67e387c2858ee13cece7de6bb25a6f204459a5b
Davi Campos <arlocm@gmail.com>
2012-08-03 10:11:38 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='cnab240',
+    version='0.01',
+    author='Tracy Web Technologies',
+    author_email='contato@tracy.com.br',
+    url='https://github.com/TracyWebTech/cnab240',
+    packages=find_packages(),
+    package_data={
+        'cnab240': ['bancos/*/*/*.json']
+    },
+    zip_safe=False,
+    install_requires=[],
+    provides=[
+        'cnab240'
+    ],
+    license='LGPL',
+    description='.',
+    long_description=open('README.md', 'r').read(),
+    download_url='https://github.com/TracyWebTech/cnab240',
+    scripts=[],
+    classifiers=[],
+    platforms='any',
+    test_suite='',
+    tests_require=[],
+)
