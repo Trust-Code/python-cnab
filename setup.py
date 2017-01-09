3dee1bce678a4673f9e148ea78c19c574c750133
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-01-09 13:24:24 -0200

    Deploy automatico no pypi (#2)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.1',
+    version='0.1.2',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
