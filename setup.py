85de2f0fa7846924329f98dec999b5d66c7736fd
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-02-03 15:47:45 -0200

    Incrementando versão

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.6',
+    version='0.1.7',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
