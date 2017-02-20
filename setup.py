da1616321a9ee136cb05f7db81f7264a100bc3ad
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-02-20 18:12:13 -0300

    Versão 1.1 do setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.7',
+    version='0.1.8',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
@@ -26,7 +26,7 @@ setup(
         ],
     },
     install_requires=[
-        'setuptools-git'
+        'setuptools-git==1.1'
     ],
     license='MIT',
     description='Lib para gerar arquivo CNAB - Integração bancária',
