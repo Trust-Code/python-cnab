27d1e4ec2cbe62adc03bfd6dbc8be7eb1c93034b
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-08-04 10:39:42 -0300

    WIP - Implementação de cnab 240 Caixa economica

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.16',
+    version='0.1.17',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
