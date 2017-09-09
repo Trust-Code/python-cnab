9d0223e5775b7bedfbb2eac2234e656ea6afbe20
Danimar Ribeiro <danimaribeiro@gmail.com>
2017-09-09 02:50:06 -0300

    Removendo arquivos não utilizados

--- cnab240/lotes/cobranca.py
+++
@@ -1,7 +0,0 @@
-
-from cnab240.tipos import LoteBase
-
-
-class LoteCobranca(LoteBase):
-    HeaderCls = LoteCobranca.banco.registros.HeaderLoteCobranca
-    TrailerCls = LoteCobranca.banco.registros.TrailerLoteCobranca
