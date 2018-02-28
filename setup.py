d633d81e9052f33fe055f589e260864f14efa1cf
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-02-28 20:20:02 -0300

    Correções para o cnab do banco bradesco

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.19',
+    version='0.1.20',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
