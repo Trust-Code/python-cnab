00434b863be8b38a1b7aa0ec0818e391a28d7965
carcaroff <johnychenjy@gmail.com>
2018-03-29 15:50:08 -0300

    [11.0][FIX(?)] JSON cecred com tag errada, remoção tag repetida

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python3-cnab',
-    version='0.9.5',
+    version='0.9.6',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
