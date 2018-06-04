f98d7e9db9a6db72a235a3cb444a2f825bbdab16
carcaroff <johnychenjy@gmail.com>
2018-06-04 17:32:42 -0300

    [WIP]Seg R multa

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-cnab',
-    version='0.1.20',
+    version='0.1.21',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
