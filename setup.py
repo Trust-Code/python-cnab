4e166c3c485046a7c2baed0b22b5004b5501de22
Danimar Ribeiro <danimaribeiro@gmail.com>
2018-12-14 10:00:02 -0200

    [DEL] Removendo arquivos antigos e fora de uso, padronização no nome de alguns campos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python3-cnab',
-    version='1.0.9',
+    version='1.0.10',
     author='Trustcode',
     author_email='suporte@trustcode.com.br',
     url='https://github.com/Trust-Code/python-cnab',
